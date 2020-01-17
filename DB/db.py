87d9a2d8eac1e762805ec49de15caf2f8b55d98a
luca de Carvalho <luca.de.carvalho@gmail.com>
2020-01-17 19:32:00 +0100

    more tables

--- DB/db.py
+++ DB/db.py
@@ -1,4 +1,6 @@
 from pyArango.connection import *
+from pyArango.collection import Collection, Field
+from pyArango.graph import Graph, EdgeDefinition
 from Decorator.singleton import Singleton
 
 
@@ -17,20 +19,37 @@ class DB(object):
             self.players_collection = self.db.createCollection(name="players")
         else:
             self.players_collection = self.db["players"]
-        if not self.db.hasCollection("questions"):
-            self.players_collection = self.db.createCollection(name="questions")
+        if not self.db.hasCollection("categories"):
+            self.categories_collection = self.db.createCollection(name="categories")
         else:
-            self.players_collection = self.db["questions"]
+            self.categories_collection = self.db["categories"]
+        if not self.db.hasCollection("questions"):
+            self.question_collection = self.db.createCollection(name="questions")
+        else:
+            self.question_collection = self.db["questions"]
         if not self.db.hasCollection("games"):
             self.games_collection = self.db.createCollection(name="games")
         else:
             self.games_collection = self.db["games"]
 
+       # if not self.db.hasGraph("TriviaGraph"):
+        #    self.graph = self.db.createGraph(name="TriviaGraph")
+        #else:
+         #   self.graph = self.db.graphs['TriviaGraph']
 
-        if not self.db.hasGraph("triviaGraph"):
-            self.graph = self.db.createGraph("triviaGraph")
-        else:
-            self.graph = self.db.graphs['triviaGraph']
+    def add_category(self, category):
+        if category not in self.categories_collection:
+            new_category = self.categories_collection.createDocument()
+            new_category["name"] = category
+            new_category._key = category
+            new_category.save()
+
+    def add_question(self, question, correct_answer, wrong_answers):
+        new_question = self.question_collection.createDocument()
+        new_question["question"] = question
+        new_question["answer"] = correct_answer
+        new_question["wrong_answers"] = wrong_answers
+        new_question.save()
 
     def add_player(self, telegram_id, name):
         if str(telegram_id) not in self.players_collection:
@@ -56,10 +75,8 @@ class DB(object):
               "RETURN c "
         bindVars = {'chatid': chat_id}
         queryResult = self.db.AQLQuery(aql, rawResults=False, batchSize=1, bindVars=bindVars)
-        #document = queryResult[0]
+
         if not queryResult:
             return False
         else:
             return True
-
-
